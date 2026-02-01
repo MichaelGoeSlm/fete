21071c36829f2f9398ea57396b2b2413c21580a8
Michael <michael@devhub.wiki>
2026-02-01 04:44:01 +0000

    FêteIci v2 with chat modal

---
+++ _next/static/V3vgUAEOzfI6kKBirRofu/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/207-45d471a3227f8d1c.js","static/chunks/pages/index-ba640ac79f2e6d7a.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
